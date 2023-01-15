d1f88a1ad6dbfc1eaa27d5a2aa99867e465f3f6b
pradeep <deepvernekar@gmail.com>
2023-01-15 16:40:27 +0530

    linear search

--- src/ds/linear/Search.kt
+++ src/ds/linear/Search.kt
@@ -1,9 +1,16 @@
 package ds.linear
 
 fun main(){
-
+    val array = arrayOf(1,2,3,5,6,7,9)
+    println(array.search(5))
 }
 
-fun binarySearch(){
-
+// linear search, time complexity is 0(n)
+fun <T> Array<T>.search(key:T):Boolean{
+    for(i in this.indices){
+        if(this[i] == key){
+            return true
+        }
+    }
+    return false
 }
